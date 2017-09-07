97e30275fc4fd90a9a695e3c04d05520244a7e75
Kristen Peck <kpeck71@gmail.com>
2017-09-07 02:02:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-	if speed =< 40 || speed => 60
+	if speed < 40 || speed > 60
 		true
 	else
 	end
@@ -8,5 +8,5 @@ end
 
 
 def not_safe?(speed)
-	speed =< 40 || speed => 60 ? true : false
+	speed < 40 || speed > 60 ? true : false
 end
